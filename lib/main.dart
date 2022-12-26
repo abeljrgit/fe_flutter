c1bec806fe3a9697058c4ac315ecdc4d2f4f489c
ALKHWARIZMI-PC\abelm <abelmanuellugtujr1996@gmail.com>
2022-12-26 12:10:44 +0800

    🚑 fix: .env loader

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app/app.dart';
 
-void main() {
+void main() async {
+  // Load .env
+  await dotenv.load(fileName: ".env");
+
   runApp(MyApp());
 }
